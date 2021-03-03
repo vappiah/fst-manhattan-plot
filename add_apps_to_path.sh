f254da8a0c05b227721f43f68840139cabee91e0
vincentappiah <appiahv@rocketmail.com>
2021-03-03 01:53:19 +0000

    Add files via upload

---
+++ add_apps_to_path.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+appsdir=$(pwd)/apps
+vcftoolsdir=$appsdir/vcftools
+
+export PATH=$PATH:$vcftoolsdir/bin
+
+export PERLLIB=$vcftoolsdir/lib:$PERLLIB
+export PERL5LIB=$vcftoolsdir/lib:$PERL5LIB
+
+export PATH=$PATH:$appsdir/mawk/bin
+
+source $(pwd)/apps/fst-env/bin/activate
